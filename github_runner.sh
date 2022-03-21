ec97ee3cc50f5d8c6f870614d63c65542f8464d5
samchan <samchan.goo.123@gmail.com>
2022-03-21 13:02:04 +0800

    init

---
+++ github_runner.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Go to github and get the runner_token
+#Add this following env to .env
+# check .env.example
+#---------------------------------------------------
+#runner_token=footoken
+#username="samchan2022"
+#repo_name="nodejs-cicd-exercise"
+#---------------------------------------------------
+
+# Load .env
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+docker run \
+   -d \
+  --restart always \
+  --name github-runner \
+  -e REPO_URL="https://github.com/$username/$repo_name" \
+  -e RUNNER_NAME="node-runner" \
+  -e RUNNER_TOKEN="${runner_token}" \
+  -e RUNNER_WORKDIR="/tmp/$repo_name" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /media/win/projects/public-github/$repo_name:/tmp/$repo_name \
+  myoung34/github-runner:latest
+
+#DEBUG only
+  #-v /home/sam/tmp/entrypoint.sh:/entrypoint.sh \
+  #-v /home/sam/tmp/config.sh:/actions-runner/config.sh \
+  #--rm \
+  #-it \
---
+++ readme.md
@@ -0,0 +1,17 @@
+Pre-requisite
+---------------------------------------------------
+- github account
+- docker
+
+Steps
+---------------------------------------------------
+1. update the .env for credential
+2. sh github_runner.sh
+
+Docs:
+---------------------------------------------------
+https://docs.github.com/en/actions/hosting-your-own-runners
+
+Runner
+---------------------------------------------------
+https://github.com/myoung34/docker-github-actions-runner#manual-org-runner
